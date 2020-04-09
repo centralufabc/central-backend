232f3c10591ec9a4f0cd0cd0b289c24e52b01a58
rodrigo98rm <mayerrodrigo98@gmail.com>
2020-04-09 00:52:11 -0300

    Tests for classes

---
+++ __tests__/integration/classes.test.js
@@ -0,0 +1,52 @@
+import request from 'supertest';
+import app from '../../src/app';
+import Class from '../../src/app/schemas/Class';
+
+describe('Classes', () => {
+	beforeAll(async () => {
+		//
+	});
+
+	afterAll(async () => {
+		await Class.deleteMany();
+	});
+
+	it('Should not accept requests with incorrect RA param', async () => {
+		// Incorrect
+		const response1 = await request(app).get('/classes/123456');
+		const response2 = await request(app).get('/classes/123456789');
+		const response3 = await request(app).get('/classes/abc');
+		const response4 = await request(app).get('/classes/123456789012');
+
+		// Correct
+		const response5 = await request(app).get('/classes/12345678');
+		const response6 = await request(app).get('/classes/12345678901');
+
+		expect(response1.status).toBe(400);
+		expect(response2.status).toBe(400);
+		expect(response3.status).toBe(400);
+		expect(response4.status).toBe(400);
+
+		expect(response5.status).toBe(200);
+		expect(response6.status).toBe(200);
+	});
+
+	it('Should return the correct classes for a given RA', async () => {
+		Class.create({
+			codigo: 'A',
+			classe: 'Geometria Analítica',
+			listaRa: ['11033320', '11022219', '11011118'],
+		});
+		Class.create({
+			codigo: 'B',
+			classe: 'Processamento da Informação',
+			listaRa: ['11033320', '11011118'],
+		});
+
+		const response1 = await request(app).get('/classes/11033320');
+		expect(response1.body.length).toBe(2);
+
+		const response2 = await request(app).get('/classes/11022219');
+		expect(response2.body.length).toBe(1);
+	});
+});
