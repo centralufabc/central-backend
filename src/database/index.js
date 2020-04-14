699968933593df937fa3547af05edf3546d1762f
Kleverson  Nascimento <kleversonnascimento@MacBook-Air-de-Kleverson.local>
2020-04-13 22:55:25 -0300

    use env var

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,18 @@ class Database {
 	}
 
 	mongo() {
-		this.mongoConnection = mongoose.connect('mongodb://localhost:27017/dev', {
+		let connectionUrl;
+		if (process.env.NODE_ENV === 'development') {
+			connectionUrl = 'mongodb://localhost:27017/central-dev2';
+		} else if (process.env.NODE_ENV === 'test') {
+			connectionUrl = 'mongodb://localhost:27017/central-dev-test';
+		} else if (process.env.NODE_ENV === 'drone_test') {
+			connectionUrl = 'mongodb://localhost:27017/dev';
+		} else {
+			connectionUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DOMAIN}/${process.env.MONGO_NAME}`;
+		}
+
+		this.mongoConnection = mongoose.connect(connectionUrl, {
 			useNewUrlParser: true,
 			useFindAndModify: true,
 			useUnifiedTopology: true,
