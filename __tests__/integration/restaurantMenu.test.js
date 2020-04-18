322f0f51a2191ae873da9fb0a614a12cebcd9e9c
Kleverson  Nascimento <kleversonnascimento@MacBook-Air-de-Kleverson.local>
2020-04-18 18:16:06 -0300

    test to update

--- __tests__/integration/restaurantMenu.test.js
+++ __tests__/integration/restaurantMenu.test.js
@@ -37,6 +37,37 @@ describe('RestaurantMenu', () => {
 
 		expect(response.status).toBe(200);
 		expect(response.body.length).toBe(1);
-		expect(response.body[0]).toBe(unfinishedMenu);
+		expect(response.body[0].lunch).toBe(unfinishedMenu.lunch);
+	});
+
+	it('Should update when they have the same date', async () => {
+		const oldMenu = {
+			day: '2030-01-20',
+			lunch: 'Strogonoff de Frango',
+			dinner: 'Strongonoff de Carne',
+			vegetarianOption: 'Kibe de PTS',
+			garnish: 'Batata Palha',
+			salads: ['Alface', 'Beterraba'],
+			desserts: ['Laranja', 'Mousse de maracujá'],
+		};
+
+		const newMenu = {
+			day: '2030-01-20',
+			lunch: 'Feijoada',
+			dinner: 'Feijoada',
+			vegetarianOption: 'Feijoada vegana',
+			garnish: 'Farofa',
+			salads: ['Alface', 'Beterraba'],
+			desserts: ['Laranja', 'Mousse de maracujá'],
+		};
+
+		await request(app).post('/restaurant/menu').send(oldMenu);
+		await request(app).post('/restaurant/menu').send(newMenu);
+
+		const response = await request(app).get('/restaurant/menu');
+
+		expect(response.status).toBe(200);
+		expect(response.body.length).toBe(1);
+		expect(response.body[0].lunch).toBe(newMenu.lunch);
 	});
 });
